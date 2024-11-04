6fa8100e399e167708167d6c51b2f4123e4ee8ce
av1nDs0uza <avinalricdsouza@gmail.com>
2024-11-04 13:49:26 +0530

    trying java

---
+++ test.java
@@ -0,0 +1,17 @@
+public class test {
+    int x = 10; 
+
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+    public static void main1(String[] args) {
+        System.out.println("Hello World 1");
+    }
+    public static void main2(String[] args) {
+        System.out.println("Hello World 2");
+    }
+    
+}
+
+
